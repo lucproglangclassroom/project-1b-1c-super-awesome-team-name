43e867288d081624818b3d8d2a2ae14e3afd1e22
Sydney Amaya <150975208+sydneyaamaya@users.noreply.github.com>
2026-02-11 21:46:34 +0000

    added topWords method in main and added other files to build upon

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,9 +1,21 @@
 package edu.luc.cs.cs371.echo
 package main
 
+import mainargs._
+
 object Main:
+
+  /*
   def main(args: Array[String]): Unit =
     val echos = Seq(new impl.SimpleEcho, new impl.DoubleEcho)
     for e <- echos do
       println(e.echo("hello"))
+  **/
+  
+  def topWords(
+    cloudSize: Int = 20
+    lengthAtLeast: Int = 6
+    windowSize: Int = 1000
+
+  )
 end Main
